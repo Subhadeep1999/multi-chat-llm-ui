86c825ef53f9b4bf53b894bd4b1d1cf76d84ff09
subhadeep1999 <subhadeepsahoo258@gmail.com>
2026-02-16 13:07:59 +0530

    chore: update Angular dependencies to version 20.3.16 and add Firebase support
    - Updated Angular packages to version 20.3.16 for improved stability and features.
    - Added Firebase dependencies for authentication and app initialization.
    - Included HttpClient module in app configuration for enhanced HTTP capabilities.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    provideHttpClient()
   ]
 };
