ad1af6e67ee09b5fc2a6b880b12a4cb876799e23
subhadeep1999 <subhadeepsahoo258@gmail.com>
2026-04-28 12:43:50 +0530

    style: update layout and styling for chat and login components; adjust environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  //apiBaseUrl: 'http://127.0.0.1:8000',
-  apiBaseUrl: 'https://multi-chat-llm-server-1.onrender.com',
-  googleClientId: '479647954804-9fs52jj5dhun0e5tsp6en7nhf9cerbhg.apps.googleusercontent.com'
+  production: false,
+  apiBaseUrl: 'http://127.0.0.1:8000',
+  //apiBaseUrl: 'https://multi-chat-llm-server-1.onrender.com',
+  googleClientId: 'GOOGLE_CLIENT_ID',
 };
