61c8a072a962b978b5eab5fe4c5a8e5dd48929fb
Subhadeep1999 <88223131+Subhadeep1999@users.noreply.github.com>
2026-09-01 11:25:21 +0530

    chore: update development environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'http://127.0.0.1:8000',
-    apiBaseUrl: 'https://multi-chat-llm-server-1.onrender.com',
-  googleClientId: '479647954804-9fs52jj5dhun0e5tsp6en7nhf9cerbhg.apps.googleusercontent.com479647954804-9fs52jj5dhun0e5tsp6en7nhf9cerbhg.apps.googleusercontent.com'
+  apiBaseUrl: 'http://127.0.0.1:8000',
+  googleClientId: '479647954804-9fs52jj5dhun0e5tsp6en7nhf9cerbhg.apps.googleusercontent.com'
 };
