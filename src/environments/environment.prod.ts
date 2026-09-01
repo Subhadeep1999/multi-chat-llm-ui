482dfc938409fae4f97eb32215371caabcbea244
Subhadeep1999 <88223131+Subhadeep1999@users.noreply.github.com>
2026-09-01 11:24:56 +0530

    chore: add production environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://multi-chat-llm-server-1.onrender.com',
+  googleClientId: '479647954804-9fs52jj5dhun0e5tsp6en7nhf9cerbhg.apps.googleusercontent.com'
+};
