fa7beacfa721c65ddc2bc3d57ba4384ba0221512
Subhadeep1999 <88223131+Subhadeep1999@users.noreply.github.com>
2026-09-01 11:26:42 +0530

    Update googleClientId to use variable client_id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://127.0.0.1:8000',
-  googleClientId: '479647954804-9fs52jj5dhun0e5tsp6en7nhf9cerbhg.apps.googleusercontent.com'
+  googleClientId: ${clent_id}
 };
